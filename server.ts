fa3992c9653520d0143851e68ef150259dbf48d4
akik4 <dupontstef80@gmail.com>
2026-05-04 17:33:20 +0200

    feat(socket): adding base socket

--- server.ts
+++ server.ts
@@ -1,7 +1,23 @@
+import { Server } from "socket.io"
+import { Server as HTTPServer} from "http"
+import http from "http"
 import app from "./src/app"
 
+const initSocket = (server: HTTPServer) => {
+  const io = new Server(server, {
+    cors : {origin: "*"}
+  });
 
-app.listen(3000, "0.0.0.0", () => {
+  io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+  })
+}
+
+const hserver = http.createServer(app) 
+
+initSocket(hserver)
+
+hserver.listen(3000, () => {
   console.log(`Server running on http://localhost:${3000}`);
 });
 
