967da70c1b9f479c0db401a09f4a5b58c0dcd39f
akik4 <dupontstef80@gmail.com>
2026-05-05 10:36:36 +0200

    feat(socket): adding socket

--- server.ts
+++ server.ts
@@ -1,18 +1,6 @@
-import { Server } from "socket.io"
-import { Server as HTTPServer} from "http"
 import http from "http"
 import app from "./src/app"
-
-const initSocket = (server: HTTPServer) => {
-  const io = new Server(server, {
-    cors : {origin: "*"}
-  });
-
-  io.on("connection", (socket) => {
-    console.log("User connected:", socket.id);
-  })
-}
-
+import { initSocket } from "./socket";
 const hserver = http.createServer(app) 
 
 initSocket(hserver)
